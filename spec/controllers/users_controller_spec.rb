9a4ec62f957389aad9219f6397729126be771a03
TC Chen <tgchen@wynetech.com>
2011-09-05 04:56:25 -0700

    add side bar for profile show page

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -10,14 +10,37 @@ describe UsersController do
     end
     
     it "should be successful" do
-      get :show, :id => @user.id
+      get :show, :id => @user
       response.should be_success
     end
     
     it "should find the right user" do
-       get :show, :id => @user.id
+       get :show, :id => @user
        assigns(:user).should == @user
     end
+    
+    it "should have the right title" do
+      get :show, :id => @user
+      response.should have_selector('title', :content => @user.name)
+    end
+    
+    it "should have the user name" do
+      get :show, :id => @user
+      response.should have_selector('h1', :content => @user.name)
+    end
+    
+    it "should have a profile image" do
+      get :show, :id =>@user
+      response.should have_selector('h1>img', :class => "gravatar")
+    end
+    
+    it "should have the right URL " do
+      get :show, :id => @user
+      response.should have_selector('td>a', :content => user_path(@user),
+                                            :href  => user_path(@user)) 
+    end
+    
+    
   end
   
   describe "GET 'new'" do
